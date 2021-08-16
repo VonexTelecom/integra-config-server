096db82987bcca5bf766f756fe1985c26eee58f9
Rafael Lopes <rafael.lopes@univision.net.br>
2021-08-16 12:36:33 -0300

    Serviço de configuração implementado

--- src/main/java/br/com/integra/config/Application.java
+++ src/main/java/br/com/integra/config/Application.java
@@ -1,8 +1,12 @@
-package br.com.integra.zuul;
+package br.com.integra.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
